0caf1c8d99961f20c73549377e2a2e3e1dbfec65
Deshavath-Venkateswara-Naik <deshavathvenkateswaranaik0193@gmail.com>
2025-07-25 03:19:44 +0530

    updated mdb

--- devTinder-Backend/src/config/database.js
+++ devTinder-Backend/src/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(process.env.DB_CONNECTION_SECRET);
+ console.log("📡 Attempting DB connection...");
   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports = connectDB;
+
